37a7beea1a63809fca60300cb7dcb4e99ed92ae5
AJ Heller <aj@drfloob.com>
2017-05-27 21:38:42 -0700

    Tests output JUnit XML reports for Jenkins to do cool stuff with

--- test/test.py
+++ test/test.py
@@ -11,4 +11,5 @@ class TestArrayPartition1(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
