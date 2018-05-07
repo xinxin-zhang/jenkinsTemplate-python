ffefe717987f87cbc30bff6dd1ce6e23b3cf71c2
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2018-05-07 15:12:16 -0700

    update

--- test/test.py
+++ test/test.py
@@ -7,7 +7,7 @@ class TestArrayPartition1(unittest.TestCase):
         self.sln = Solution()
 
     def test_basic(self):
-        self.assertEqual(self.sln.ArrayPairSum([1,5,2,3]), 4)
+        self.assertEqual(self.sln.ArrayPairSum([1,5,2,3]), 6)
 	
 
 if __name__ == '__main__':
